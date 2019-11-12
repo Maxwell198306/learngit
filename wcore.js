3eea16fe0b6f57e6521808a5e978eefc876f5d1a
e <307159189@qq.com>
2019-11-12 11:48:59 +0800

    add 2 files

---
+++ wcore.js
@@ -0,0 +1,16 @@
+﻿目前，在GitHub上的这个learngit仓库还是空的，GitHub告诉我们，可以从这个仓库克隆出新的仓库，也可以把一个已有的本地仓库与之关联，然后，把本地仓库的内容推送到GitHub仓库。
+
+现在，我们根据GitHub的提示，在本地的learngit仓库下运行命令：
+
+$ git remote add origin git@github.com:michaelliao/learngit.git
+
+请千万注意，把上面的michaelliao替换成你自己的GitHub账户名，否则，你在本地关联的就是我的远程库，关联没有问题，但是你以后推送是推不上去的，因为你的SSH Key公钥不在我的账户列表中。
+
+添加后，远程库的名字就是origin，这是Git默认的叫法，也可以改成别的，但是origin这个名字一看就知道是远程库。
+
+下一步，就可以把本地库的所有内容推送到远程库上：
+
+作者：CoderTung
+链接：https://www.jianshu.com/p/561a4fec4a96
+来源：简书
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
